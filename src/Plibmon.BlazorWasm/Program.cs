1d34483c45b899485c3fbafa783eaa568d6743cd
BradR <1196817+bradrhodes@users.noreply.github.com>
2022-02-04 20:24:10 -0600

    Getting Blazor hooked up with the API

--- src/Plibmon.BlazorWasm/Program.cs
+++ src/Plibmon.BlazorWasm/Program.cs
@@ -2,12 +2,15 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Plibmon.BlazorWasm;
 using Plibmon.Domain;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiBaseUrl = builder.Configuration.GetValue<string>("ApiUrl");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddPlibmonDomain();
 builder.Services.AddPlibmonSampleConfig();
 await builder.Build().RunAsync();
