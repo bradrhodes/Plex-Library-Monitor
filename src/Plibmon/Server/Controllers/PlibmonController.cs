15c28149b6b98a9c3e7a33ec73e18fdf3b91646e
BradR <1196817+bradrhodes@users.noreply.github.com>
2022-02-05 00:59:06 -0600

    Copied the remaining stuff from the old projects.

---
+++ src/Plibmon/Server/Controllers/PlibmonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Plibmon.Domain;
+
+namespace Plibmon.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlibmonController : Controller
+{
+    private readonly IPlibmonService _plibmon;
+
+    public PlibmonController(IPlibmonService plibmon)
+    {
+        _plibmon = plibmon ?? throw new ArgumentNullException(nameof(plibmon));
+    }
+    
+    // GET: api/Plibmon
+    [HttpGet]
+    public Task<bool> Get(CancellationToken cancellationToken)
+    {
+        return _plibmon.CanConnectToPlex(cancellationToken);
+    }
+}
--- src/Plibmon/Server/Program.cs
+++ src/Plibmon/Server/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.ResponseCompression;
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Plibmon.Domain;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+
+builder.Services.AddPlibmonDomain();
+// Todo: this needs to be removed
+builder.Services.AddPlibmonSampleConfig();
+
 builder.Services.AddHangfire(x => x.UseMemoryStorage());
 builder.Services.AddHangfireServer();
 
