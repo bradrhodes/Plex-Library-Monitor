fa539653c55483a121dd2b2a669a941423074528
Brad Rhodes <1196817+bradrhodes@users.noreply.github.com>
2022-02-10 14:15:11 -0600

    Added a note about overriding with Environment vars

--- src/Plibmon.Domain/DomainDiRegistration.cs
+++ src/Plibmon.Domain/DomainDiRegistration.cs
@@ -13,6 +13,9 @@ public static class DomainDiRegistration
 {
    public static void AddPlibmonDomain(this IServiceCollection services, WebApplicationBuilder builder)
    {
+      // Note: If you need to override the values in the appSettings.json file, they can be overridden via
+      // Environment vars.  Naming uses double __.  Ex. To override the ClientId, create and set an
+      // Environment var named 'Plibmon__ClientId'.
       services.AddSingleton<PlibmonSettings>(_ =>
          builder.Configuration.GetSection(PlibmonSettings.ConfigSectionName).Get<PlibmonSettings>());
       services.AddSingleton<FileStorageSettings>(provider =>
