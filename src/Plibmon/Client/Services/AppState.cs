a4bd5dc3998d9cac340ecfefff64db6533b03126
Brad Rhodes <1196817+bradrhodes@users.noreply.github.com>
2022-02-26 00:25:22 -0600

    Added browser app state and used it to hide the connect to plex menu item when a connection is available

---
+++ src/Plibmon/Client/Services/AppState.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Plibmon.Client.Services;
+
+/// <summary>
+/// This is a pattern for state in Blazor shown on Carl Franklin's Blazor Train
+/// https://youtu.be/BB4lK2kfKf0?t=546 
+/// </summary>
+public class AppState
+{
+   public event Action<ComponentBase, string> StateChanged;
+   private void NotifyStateChanged(ComponentBase source, string property) 
+      => StateChanged?.Invoke(source, property);
+
+   public bool CanConnectToPlex { get; private set; }
+
+   public void UpdateCanConnectToPlex(ComponentBase source, bool value)
+   {
+      CanConnectToPlex = value;
+      NotifyStateChanged(source, "CanConnectToPlex");
+   }
+}
